5513638a27a806e0c860367fc56439e5aa907c3b
jann.e <janne@karlstadmultisport.se>
2014-05-05 15:46:56 +0200

    Diagram code ok

--- app/src/main/java/se/racetime/homeautomation/app/MainActivity.java
+++ app/src/main/java/se/racetime/homeautomation/app/MainActivity.java
@@ -3,6 +3,7 @@ package se.racetime.homeautomation.app;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,10 @@ import android.widget.Toast;
 //import com.jjoe64.graphview.GraphView;
 //import com.jjoe64.graphview.GraphViewSeries;
 //import com.jjoe64.graphview.LineGraphView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -29,29 +34,41 @@ import org.json.JSONStringer;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    enum HttpTask { basic, idHour };
+    GraphView graphView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        // init example series data
-        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphView.GraphViewData[] {
-                new GraphView.GraphViewData(1, 2.0d)
-                , new GraphView.GraphViewData(2, 1.5d)
-                , new GraphView.GraphViewData(3, 2.5d)
-                , new GraphView.GraphViewData(4, 1.0d)
-        });
+        graphView = new LineGraphView(this,"Temperature");
 
-        GraphView graphView = new LineGraphView(
-                this // context
-                , "GraphViewDemo" // heading
-        );
-        graphView.addSeries(exampleSeries); // data
+        /*
+        {
+            @Override
+            protected String formatLabel(double value, boolean isValueX)
+            {
+                if (isValueX)   // transform number to time
+                    return dateTimeFormatter.format(new Date((long) value*1000));
+                else
+                    return super.formatLabel(value, isValueX);
+            }
+        };*/
+
+
+        /*        // init example series data
+
+
 
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.test);
@@ -84,25 +101,25 @@ public class MainActivity extends ActionBarActivity
     {
         switch (v.getId()) {
             case R.id.buttonTempAttic:
-                new ReadTemperatureIdJSONFeedTask().execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/9/12");
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/9/12");
                 break;
             case R.id.buttonTempBasement:
-                // do something else
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/43/12");
                 break;
             case R.id.buttonTempInside:
-                // i'm lazy, do nothing
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/151/12");
                 break;
             case R.id.buttonTempOutside1:
-                // do something else
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/181/12");
                 break;
             case R.id.buttonTempOutside2:
-                // do something else
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/140/12");
                 break;
             case R.id.buttonTempPadio:
-                // do something else
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/180/12");
                 break;
             case R.id.buttonTempUnderhouse:
-                // do something else
+                new ReadTemperatureJSONFeedTask(HttpTask.idHour).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature/139/12");
                 break;
         }
 
@@ -118,7 +135,7 @@ public class MainActivity extends ActionBarActivity
         Button button = (Button)v;
         CharSequence text = button.getText();
 
-        new ReadTemperatureJSONFeedTask().execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature");
+        new ReadTemperatureJSONFeedTask(HttpTask.basic).execute("http://racetime.no-ip.org:8080/tellstickservice/Temperature");
     }
 
     public String readJSONFeed(String URL)
@@ -158,12 +175,73 @@ public class MainActivity extends ActionBarActivity
 
     private class ReadTemperatureJSONFeedTask extends AsyncTask<String, Void, String>
     {
+        private HttpTask httpTask;
+
+        public ReadTemperatureJSONFeedTask(HttpTask _httpTask)
+        {
+            httpTask = _httpTask;
+        }
+
         protected String doInBackground(String... urls)
         {
             return readJSONFeed(urls[0]);
         }
 
         protected void onPostExecute(String result)
+        {
+            try
+            {
+                if(httpTask == HttpTask.basic)
+                    UpdateAllTemperatures(result);
+                else if(httpTask == HttpTask.idHour)
+                    UpdateTemperatureDiagram(result);
+
+            }
+            catch (Exception e)
+            {
+                Log.d("ReadTemperatureJSONFeedTask", e.getLocalizedMessage());
+            }
+        }
+
+        private void UpdateTemperatureDiagram(String result)
+        {
+            try
+            {
+                JSONArray jsonArray = new JSONArray(result);
+
+                //final java.text.DateFormat dateTimeFormatter = DateFormat.getTimeFormat(mActivity);
+
+                GraphView.GraphViewData graphViewData[] = new GraphView.GraphViewData[jsonArray.length()];
+
+                for(int i = 0; i < jsonArray.length(); i++)
+                {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String sensorId = jsonObject.getString("sensorId");
+                    String temperature = jsonObject.getString("temperature");
+                    String humidity = jsonObject.getString("humidity");
+                    String createdDate = jsonObject.getString("createDate");
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = sdf.parse(createdDate);
+                    long millisecond = date.getTime();
+
+                    graphViewData[i] = new GraphView.GraphViewData(millisecond, new Double(temperature.replace(',','.')));
+                }
+
+
+                GraphViewSeries exampleSeries = new GraphViewSeries(graphViewData);  // init data
+                graphView.addSeries(exampleSeries); // data
+
+                LinearLayout layout = (LinearLayout) findViewById(R.id.test);
+                layout.addView(graphView);
+            }
+            catch (Exception e)
+            {
+                Log.d("ReadTemperatureJSONFeedTask", e.getLocalizedMessage());
+            }
+        }
+
+        private void UpdateAllTemperatures(String result)
         {
             try
             {
@@ -203,54 +281,4 @@ public class MainActivity extends ActionBarActivity
             }
         }
     }
-
-    private class ReadTemperatureIdJSONFeedTask extends AsyncTask<String, Void, String>
-    {
-        protected String doInBackground(String... urls)
-        {
-            return readJSONFeed(urls[0]);
-        }
-
-        protected void onPostExecute(String result)
-        {
-            try
-            {
-                JSONArray jsonArray = new JSONArray(result);
-
-                for(int i = 0; i < jsonArray.length(); i++)
-                {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    String sensorId = jsonObject.getString("sensorId");
-                    String temperature = jsonObject.getString("temperature");
-                    String humidity = jsonObject.getString("humidity");
-                    String createdDate = jsonObject.getString("createDate");
-
-                    TextView textViewTotal = null;
-                    if(sensorId == "140")
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempOutside2);
-                    else if(sensorId.contains("139"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempUnderhouse);
-                    else if(sensorId.contains("180"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempPadio);
-                    else if(sensorId.contains("9"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempAttic);
-                    else if(sensorId.contains("151"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempInside);
-                    else if(sensorId.contains("181"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempOutside1);
-                    else if(sensorId.contains("43"))
-                        textViewTotal = (TextView)findViewById(R.id.textViewTempBasement);
-
-                    if(textViewTotal != null)
-                        textViewTotal.setText(temperature);
-                }
-            }
-            catch (Exception e)
-            {
-                Log.d("ReadTemperatureJSONFeedTask", e.getLocalizedMessage());
-            }
-        }
-    }
-
-
 }
